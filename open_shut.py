d262f0f4d3698cdaa1342ab3398aa902813ffdc0
Jason Stajich <jason.stajich@gmail.com>
2019-10-22 23:51:58 -0700

    template for open-shut

---
+++ open_shut.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os,csv,gzip
+
+# write a script to create new file called closed.txt
+
+outfile="closed.txt"
+# on the HPCC cluster you can use the file
+file="/bigdata/gen220/shared/simple/title.basics.tsv.gz"
+# or if you run this on your own computer use this
+#file="title.basics.tsv.gz"
+
+
+if not os.path.exists(file):
+    os.system("curl -O https://datasets.imdbws.com/title.akas.tsv.gz")
+
+with gzip.open(file,"r") as fh:
+    # now use the csv reader to read in the file, delimiter is '\t'
