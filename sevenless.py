bb79fb5c088fa745a072bb273921323fa262b7ed
zchah001 <72586266+zchah001@users.noreply.github.com>
2020-12-09 20:36:35 -0800

    Update sevenless.py

--- sevenless.py
+++ sevenless.py
@@ -1,7 +1,12 @@
-# Prints a row of numbers from 0-99, excluding those divisible by 7
+#!/usr/bin/env python3
+#Print out values from 0 to 99 except those  perfectly divisible by 7
 
-for x in range(0,100):
-    if x % 7 != 0:
-        print (x);
-    else:
-        pass
+Start = 0
+End   = 99
+divisor=7
+print("Print out  numbers from",Start,"to",End, " not divisible by",divisor)
+for Sevenless  in range(0, 99):
+     if Sevenless  % 7 != 0:
+                print(Sevenless)
+done
+
