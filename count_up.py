b892759c7f803bd2e98bc674b7c2933e47c9b041
zchah001 <72586266+zchah001@users.noreply.github.com>
2020-12-09 20:37:23 -0800

    Update count_up.py

--- count_up.py
+++ count_up.py
@@ -1,44 +1,68 @@
-from sys import argv
+#!/usr/bin/env python3
 
-if len(argv) != 3:
-	print("Run with 2 arguments: input_GFF.gff3 output_FASTA.fasta")
-	exit(0)
-else:
-	pass
 
-script, input_gff, input_fasta = argv
+# Download the required files using curl
 
-gff_file = open(input_gff)
-fasta_file = open(input_fasta)
+gff="Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.37.gff3.gz"
+fasta="Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz"
 
-gene_count = 0
-gene_length_total = 0
+import os,gzip,itertools,csv,re
 
-# Go through lines in GFF.
-for line in gff_file:
-	if "#" in line:
-		continue
+# To parse FASTA files
+# define what a header looks like in FASTA format
+def isheader(line):
+    return line[0] == '>'
 
-	current_line = line.split('\t')
-	if gene in current_line[2]:
-		gene_count += 1
-		start = current_line[3]
-		end = current_line[4]
-		gene_length = end - start
-		gene_length_total += gene_length
-	
-	
-print(f"The number of genes is {gene_count}.")
-print(f"The total gene length is {gene_length_total}.")
-		
-line_length_total = 0	
-	
-for line in fasta_file:
-	if ">" in line:
-		continue
-		
-	line_length = len(line)
-	line_length_total += line_length
-	
-print(f"The total length of the genome is {line_length_total}.")
-		
+def aspairs(f):
+    seq_id = ''
+    sequence = ''
+    for header,group in itertools.groupby(f, isheader):
+        if header:
+            line = next(group)
+            seq_id = line[1:].split()[0]
+        else:
+            sequence = ''.join(line.strip() for line in group)
+            yield seq_id, sequence
+            
+if not os.path.exists(gff):
+    os.system("curl -O ftp://ftp.ensemblgenomes.org/pub/bacteria/release-45/gff3/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.37.gff3.gz")
+
+if not os.path.exists(fasta):
+    os.system("curl -O ftp://ftp.ensemblgenomes.org/pub/bacteria/release-45/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/dna/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz")
+    
+
+# Process gff to get gene count, total gene length and coding length
+
+with gzip.open(gff, "rt") as fh:
+
+# now add code to process this
+
+    gff = csv.reader(fh, delimiter="\t")
+    Num_of_Genes = 0
+    Total_Gene_Length = 0
+    Total_Exon_Length = 0
+    for row in gff:
+        if row[0].startswith("#"):
+                continue
+
+        if row[2] == 'gene':
+            Num_of_Genes += 1
+            Total_Gene_Length += int(row[4]) - int(row[3])
+
+        if row[2] == 'CDS':
+            Total_Exon_Length += int(row[4]) - int(row[3])
+
+
+# Process fasta to get total genome length
+
+with gzip.open(fasta, "rt") as fh:
+     pairs = aspairs(fh)
+     seqs = dict(pairs)
+     Total_Genome_Length = len(seqs['Chromosome'])
+
+
+# Print results
+print('Number of genes: {}'.format(Num_of_Genes))
+print('Total length of the genes is: {}'.format(Total_Gene_Length))
+print('Total length of genome is: {}'.format(Total_Genome_Length))
+print('percentage of the genome which is coding: {:.2f}%'.format(100 * (Total_Exon_Length/Total_Genome_Length)))
